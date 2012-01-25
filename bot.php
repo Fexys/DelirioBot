80e5d185e25bc5ebefa0b10a0e4983caea8f54a8
Mte90 <mte90smanettator@gmail.com>
2012-01-25 22:14:19 +0100

    tab & supporto file op

--- bot.php
+++ bot.php
@@ -1,235 +1,232 @@
 <?php
-   include_once('SmartIRC.php');
+include_once( 'SmartIRC.php' );
 
 /* Variabile che contiene il nome del chan */
- $chan = "#DeliriNotturni";
 
- //$chan = "#delirinotturni2";
+$chan = "#DeliriNotturni";
 
- /*Next, create the bot-class:*/
+//$chan = "#delirinotturni2";
+//*Next, create the bot-class:*/
+
 class Delirio {
 
-/*A Greet function*/
+	public $op;
 
-    function onjoin_greeting(&$irc, &$data)
-    { // if we join, don't greet ourself, just jump out via return
-	if ($data->nick == $irc->_nick)
-	   return;
-
-	// now check if this is the right channel
-	if ($data->channel == $chan)
-	// it is, lets greet the joined user
-	   $irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, 'hi '.$data->nick);
-    }
-
-
-/*Quit Function*/
-
-    function quit(&$irc, &$data)
-    {
-	// Only run the command if the nick is an owner.
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-		$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "Addio mondo crudele!");
-		exit();
-		Return ;
+	function setVar( ) {
+		$op = file( 'op.php' );
 	}
-    }
 
-/*Kick-Function*/
+	/*A Greet function*/
 
-    function kick(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1],$data->messageex[2])) {
-		$nickname = $data->messageex[1];
-		$reason = $data->messageex[2];
-		$channel = $data->channel;
-		$irc->kick($channel, $nickname, $reason);
-	    } else {
-		$irc->message( $data->type, $data->nick, 'Invalid Parameter' );
-		$irc->message( $data->type, $data->nick, 'use: !kick $nick' );
-	    }
+	function onjoin_greeting( &$irc, &$data ) {
+		// if we join, don't greet ourself, just jump out via return
+
+		if( $data->nick == $irc->_nick ) {
+			return;
+		}
+
+		// now check if this is the right channel
+
+		if( $data->channel == $data->channel ) {
+			$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, 'hi '.$data->nick );
+		}
 	}
-    }
 
-/*If the bot gets kicked, let it rejoin*/
+	/*Quit Function*/
 
-    function kick_response(&$irc, &$data)
-    { //if bot is kicked
-	  if ($data->nick == $irc->_nick) {
-	   $irc->join(array($chan));
-	   $irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "dont kick me... please");
-	   Return ;
-	  }
-    }
-
-
-/*Function to change channelmodes*/
-
-    function mode($channel, $newmode = null, $priority = SMARTIRC_MEDIUM)
-    {
-	  if ($newmode !== null) {
-	  $irc->_send('MODE '.$channel.' '.$newmode, $priority);
-	  } else {
-	  $irc->_send('MODE '.$channel, $priority);
-	  }
-    }
-
-/*Devoice Function*/
-
-    function devoice(&$irc, &$data)
-    {
-	  if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	  if(isset($data->messageex[1])) {
-	  $nickname = $data->messageex[1];
-	   $channel = $data->channel;
-	   $irc->devoice($channel, $nickname );
-	  }
-	  }
-    }
-
-/*Op Function*/
-
-    function op(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$nickname = $data->messageex[1];
-		$channel = $data->channel;
-		$irc->op($channel, $nickname );
-	    }
+	function restart( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, "Addio mondo crudele!" );
+			exit( );
+			Return;
+		}
 	}
-    }
 
-/*Deop Function*/
+	/*Kick-Function*/
 
-    function deop(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$nickname = $data->messageex[1];
-		$channel = $data->channel;
-		$irc->deop($channel, $nickname );
-	    }
+	function kick( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1], $data->messageex[2] ) ) {
+				$nickname = $data->messageex[1];
+				$reason   = $data->messageex[2];
+				$channel  = $data->channel;
+				$irc->kick( $channel, $nickname, $reason );
+			}
+			else {
+				$irc->message( $data->type, $data->nick, 'Invalid Parameter' );
+				$irc->message( $data->type, $data->nick, 'use: !kick $nick' );
+			}
+		}
 	}
-    }
 
-/*Join Function*/
+	/*If the bot gets kicked, let it rejoin*/
 
-    function join(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$channel = $data->messageex[1];
-		$irc->join($channel);
-	    }
+	function kick_response( &$irc, &$data ) {
+		//if bot is kicked
+
+		if( $data->nick == $irc->_nick ) {
+			$irc->join( array( $chan ) );
+			$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, "dont kick me... please" );
+			Return;
+		}
 	}
-    }
 
-/*Part Function*/
+	/*Function to change channelmodes*/
 
-    function part(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$channel = $data->messageex[1];
-		$irc->part($channel);
-	    }
+	function mode( $channel, $newmode = null, $priority = SMARTIRC_MEDIUM ) {
+		if( $newmode !== null ) {
+			$irc->_send( 'MODE '.$channel.' '.$newmode, $priority );
+		}
+		else {
+			$irc->_send( 'MODE '.$channel, $priority );
+		}
 	}
-    }
 
-/*Function to rejoin a channel*/
+	/*Devoice Function*/
 
-    function rejoin(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$channel = $data->messageex[1];
-		$irc->part($channel);
-		$irc->join($channel);
-	    }
+	function devoice( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$nickname = $data->messageex[1];
+				$channel = $data->channel;
+				$irc->devoice( $channel, $nickname );
+			}
+		}
 	}
-    }
 
-/*Ban Function*/
+	/*Op Function*/
 
-    function ban(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$hostmask = $data->messageex[1];
-		$channel = $data->channel;
-		$irc->ban($channel, $hostmask);
-	    } else {
-		$irc->message( $data->type, $data->nick, 'Invalid Parameter' );
-		$irc->message( $data->type, $data->nick, 'use: !ban $nick' );
-	    }
+	function op( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$nickname = $data->messageex[1];
+				$channel = $data->channel;
+				$irc->op( $channel, $nickname );
+			}
+		}
 	}
-    }
 
-/*Function for the nickchange-command*/
+	/*Deop Function*/
 
-    function nick(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$newnick = $data->messageex[1];
-		$channel = $data->channel;
-		$irc->changeNick($newnick );
-	    }
+	function deop( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$nickname = $data->messageex[1];
+				$channel = $data->channel;
+				$irc->deop( $channel, $nickname );
+			}
+		}
 	}
-    }
 
-    function saluta(&$irc, &$data)
-    {
-	if($data->nick == "Mte90" || $data->nick == "PTKDev" || $data->nick == "Fexys") {
-	    if(isset($data->messageex[1])) {
-		$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "Fottiti ".$data->messageex[1]);
-	    }
+	/*Join Function*/
+
+	function join( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$channel = $data->messageex[1];
+				$irc->join( $channel );
+			}
+		}
 	}
-    }
 
-/*Function that does the actual nickchange*/
+	/*Part Function*/
 
-    function changeNick($newnick, $priority = SMARTIRC_MEDIUM)
-    {
-	$this->_send('NICK '.$newnick, $priority);
-	$this->_nick = $newnick;
-    }
+	function part( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$channel = $data->messageex[1];
+				$irc->part( $channel );
+			}
+		}
+	}
 
-/*End the Bot-class*/
+	/*Function to rejoin a channel*/
 
+	function rejoin( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$channel = $data->messageex[1];
+				$irc->part( $channel );
+				$irc->join( $channel );
+			}
+		}
+	}
+
+	/*Ban Function*/
+
+	function ban( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$hostmask = $data->messageex[1];
+				$channel = $data->channel;
+				$irc->ban( $channel, $hostmask );
+			}
+			else {
+				$irc->message( $data->type, $data->nick, 'Invalid Parameter' );
+				$irc->message( $data->type, $data->nick, 'use: !ban $nick' );
+			}
+		}
+	}
+
+	/*Function for the nickchange-command*/
+
+	function nick( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$newnick = $data->messageex[1];
+				$channel = $data->channel;
+				$irc->changeNick( $newnick );
+			}
+		}
+	}
+
+	function saluta( &$irc, &$data ) {
+		if( in_array( $data->nick, $this->$op ) ) {
+			if( isset( $data->messageex[1] ) ) {
+				$irc->message( SMARTIRC_TYPE_CHANNEL, $data->channel, "Fottiti ".$data->messageex[1] );
+			}
+		}
+	}
+
+	/*Function that does the actual nickchange*/
+
+	function changeNick( $newnick, $priority = SMARTIRC_MEDIUM ) {
+		$this->_send( 'NICK '.$newnick, $priority );
+		$this->_nick = $newnick;
+	}
+
+	/*End the Bot-class*/
 }
 
 /*Start the bot and set some settings*/
 
-$bot = new Delirio();
-$irc = new Net_SmartIRC();
-$irc->setDebug(SMARTIRC_DEBUG_ALL);
-$irc->setUseSockets(TRUE);
+$bot = new Delirio( );
+$irc = new Net_SmartIRC( );
+$irc->setDebug( SMARTIRC_DEBUG_ALL );
+$irc->setUseSockets( TRUE );
 
 /*Bind IRC Commands to the above defined functions and end the PHP-file*/
 
-$irc->registerActionhandler(SMARTIRC_TYPE_JOIN, '.*', $bot, 'onjoin_greeting');
-$irc->connect('irc.freenode.org', 6667);
-$irc->registerActionhandler(SMARTIRC_TYPE_KICK, '.*', $bot, 'kick_response');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '!restart', $bot, 'quit');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!kick', $bot, 'kick');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!voice', $bot, 'voice');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!devoice', $bot, 'devoice');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!op', $bot, 'op');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!deop', $bot, 'deop');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!join', $bot, 'join');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!part', $bot, 'part');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!rejoin', $bot, 'rejoin');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!ban', $bot, 'ban');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!nick', $bot, 'nick');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!saluta', $bot, 'saluta');
+$irc->registerActionhandler( SMARTIRC_TYPE_JOIN, '.*', $bot, 'onjoin_greeting' );
+$irc->connect( 'irc.freenode.org', 6667 );
+$irc->registerActionhandler( SMARTIRC_TYPE_KICK, '.*', $bot, 'kick_response' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '!restart', $bot, 'restart' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!kick', $bot, 'kick' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!voice', $bot, 'voice' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!devoice', $bot, 'devoice' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!op', $bot, 'op' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!deop', $bot, 'deop' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!join', $bot, 'join' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!part', $bot, 'part' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!rejoin', $bot, 'rejoin' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!ban', $bot, 'ban' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!nick', $bot, 'nick' );
+$irc->registerActionhandler( SMARTIRC_TYPE_CHANNEL, '^!saluta', $bot, 'saluta' );
 
 // nick , nome , realname , ident, senha do nick
-$irc->login('ilDelirante', 'ilDelirante'.'delirio', 8, 'delirioNetwork','');
-$irc->join($chan);
+
+$irc->login( 'ilDelirante', 'ilDelirante'.'delirio', 8, 'delirioNetwork', '' );
+$irc->join( $chan );
 $irc->listen( );
 $irc->disconnect( );
-
 ?>
