bc2f4a162596db8ecbda654b180556ef25c0df81
Mte90 <mte90smanettator@gmail.com>
2012-01-25 23:33:05 +0100

    mte90,vs,gianguido,worst,toketin

--- bio.php
+++ bio.php
@@ -6,4 +6,10 @@ $bio['Mte90']['bio']="Fan sfegatato di Debian e KDE.Superpotere: Sente la puzza 
 
 $bio['v2']['bio']="Nome in codice: V2.Ricercata e pericolosa Cyberpunker, psicolabile e squilibrata. ";
 
+$bio['gianguido']['bio']="I was chatting here, but then i took an arrow to the knee";
+
+$bio['worst']['bio']="Nerd mancato, apprendista smanettone, socialmente impedito. Innamorato di Debian&Openbox, detesta tutto ciò che è imposto.";
+
+$bio['toketin']['bio']="Arciere convinto, studente universitario a Udine e juventiro. Amo la configurabilità e la leggerezza";
+
 ?>
