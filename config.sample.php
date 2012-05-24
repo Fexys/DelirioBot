8770199230d102db428432e985681abc4f75a3b7
Fexys <marcopat@gmail.com>
2012-05-24 18:34:14 +0200

    Polish config

--- config.sample.php
+++ config.sample.php
@@ -5,14 +5,14 @@ date_default_timezone_set('Europe/Rome');
 define('LOG_DIR', 'logs/');
 define('COMMAND_DIR', 'commands/');
 
-$config['database']['host'] = 'localhost';
+$config['database']['host'] = 'host';
 $config['database']['user'] = 'user';
 $config['database']['name'] = 'name';
 $config['database']['password'] = 'password';
 
-$config['server']	= 'irc.freenode.net';
-$config['port']		= 6667;
-$config['channel']	= '#DeliriNotturni';
+$config['server'] = 'irc.freenode.net';
+$config['port'] = 6667;
+$config['channel'] = '#DeliriNotturni';
 
 $config['bot']['nickname'] = 'ilDelirante';
 $config['bot']['realname'] = 'ilDelirante [ver ' . VERSION . ']';
